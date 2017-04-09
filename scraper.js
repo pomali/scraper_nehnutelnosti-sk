8275d7684789cba8fe9fbb3460cb60404d4dc9b7
pom <pom@poms-MacBook-Pro.local>
2017-04-09 17:02:47 +0200

    Initial

--- scraper.js
+++ scraper.js
@@ -8,21 +8,21 @@ function initDatabase(callback) {
 	// Set up sqlite database.
 	var db = new sqlite3.Database("data.sqlite");
 	db.serialize(function() {
-		db.run("CREATE TABLE IF NOT EXISTS data (name TEXT)");
+		db.run("CREATE TABLE IF NOT EXISTS data (name TEXT, price REAL, area REAL, location TEXT, photo TEXT)");
 		callback(db);
 	});
 }
 
-function updateRow(db, value) {
+function updateRow(db, name, price, area, location, photo) {
 	// Insert some data.
-	var statement = db.prepare("INSERT INTO data VALUES (?)");
-	statement.run(value);
+	var statement = db.prepare("INSERT INTO data VALUES (?, ?, ?, ?, ?)");
+	statement.run(name, price, area, location, photo);
 	statement.finalize();
 }
 
 function readRows(db) {
 	// Read some data.
-	db.each("SELECT rowid AS id, name FROM data", function(err, row) {
+	db.each("SELECT rowid AS id, name, price, area, location, photo FROM data", function(err, row) {
 		console.log(row.id + ": " + row.name);
 	});
 }
@@ -41,13 +41,23 @@ function fetchPage(url, callback) {
 
 function run(db) {
 	// Use request to read in pages.
-	fetchPage("https://morph.io", function (body) {
+	fetchPage("http://www.nehnutelnosti.sk/bratislava-iv-karlova-ves/byty/predaj?p%5Bdistance%5D=10&p%5Bparam1%5D%5Bfrom%5D=60000&p%5Bparam1%5D%5Bto%5D=150000&p%5Bparam11%5D%5Bfrom%5D=50&p%5Bparam11%5D%5Bto%5D=150&p%5Bfoto%5D=1&p%5Bcategories%5D%5Bids%5D=10003.10004.10005.10006", function (body) {
 		// Use cheerio to find things in the page with css selectors.
 		var $ = cheerio.load(body);
 
-		var elements = $("div.media-body span.p-name").each(function () {
-			var value = $(this).text().trim();
-			updateRow(db, value);
+		var elements = $("#inzeraty div.inzerat").each(function (i,el) {
+            var $el = $(el)
+            var $headLink = $el.find(".advertisement-head h2>a")
+            var $content = $el.find(".inzerat-content")
+
+            var name = $headLink.text()
+            var url = $headLink.attr("href")
+            var location = $content.find(".locationText").text() || 'missing location'
+            var area = $content.find(".estate-area span.red").text() || 0
+            var price = $el.find(".advertisement-rightpanel cena span.red").text().replace(/\D/g,'')
+            var photo = $el.find(".advertisement-photo img").attr("data-src") || 'no-photo' 
+
+			updateRow(db, name, url, photo, price, area, location);
 		});
 
 		readRows(db);
